d45a338072e466b114223750a3c036cebcdc9398
Sarvjot <sarvjotmalhotra@gmail.com>
2026-01-22 13:48:00 +0530

    01 folder

---
+++ 01_folder/src/app.jsx
@@ -0,0 +1,15 @@
+
+const app = () => {
+  return (
+<>
+
+    <div id="parent">app
+     <h1 id="child1">app</h1>
+     <h3 id="chld2">hello</h3> 
+     </div>
+     <div id="chahcha"> </div>
+</>
+  )
+}
+
+export default app
