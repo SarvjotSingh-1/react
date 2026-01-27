d05ccc3ca0e854dfaff3bb50f76fc0c572288077
Sarvjot <sarvjotmalhotra@gmail.com>
2026-01-27 15:44:08 +0530

    First Commit

---
+++ 13-notes-app/src/App.jsx
@@ -0,0 +1,106 @@
+import { useState } from 'react'
+import { TrashIcon } from '@heroicons/react/24/solid';
+
+const App = () => {
+
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+
+  const [task, setTask] = useState([])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+    const copyTask = [...task];
+
+    copyTask.push({ title, details })
+
+    setTask(copyTask)
+
+    setTitle('')
+    setDetails('')
+  }
+
+
+  const deleteNote = (idx) => {
+    const copyTask = [...task];
+
+    copyTask.splice(idx, 1)
+
+    setTask(copyTask)
+  }
+
+  return (
+    <div className='h-screen lg:flex bg-black text-white'>
+
+      <form onSubmit={(e) => {
+        submitHandler(e)
+      }} className='flex gap-4 lg:w-1/2 p-10 flex-col items-start'>
+
+        <h1 className='text-4xl mb-2 font-bold'>Add Notes</h1>
+
+        
+        <input
+          type="text"
+          placeholder='Enter Notes Heading'
+          className='px-5 w-full font-medium py-2 border-2 outline-none rounded '
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value)
+          }}
+        />
+
+        
+        <textarea
+          type="text"
+          className='px-5 w-full font-medium h-32 py-2 flex items-start flex-row border-2 outline-none  rounded '
+          placeholder='Write Details here'
+          value={details}
+          onChange={(e) => {
+            setDetails(e.target.value)
+          }}
+        />
+
+        <button
+          className='bg-white active:scale-95 font-medium w-full outline-none  text-black px-5 py-2 rounded'
+        >
+          Add Note
+        </button>
+
+      </form>
+      <div className='lg:w-1/2 lg:border-l-2  p-10'>
+        <h1 className='text-4xl font-bold'>Recent Notes</h1>
+        <div className='flex flex-wrap items-start justify-start gap-5 mt-6 h-[90%] overflow-auto'>
+          {task.map(function (elem, idx) {
+
+            return <div key={idx} className=" flex justify-between flex-col items-start relative h-52 w-40 bg-cover rounded-xl text-black pt-9 pb-4 px-4 bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')]">
+              <div>
+                <h3 className='leading-tight text-lg font-bold'>{elem.title}</h3>
+                <p className='mt-2 leading-tight text-xs font-semibold text-gray-600'>{elem.details}</p>
+              </div>
+
+
+<button
+  onClick={() => deleteNote(idx)}
+  className="flex items-center justify-center gap-2 w-full cursor-pointer bg-red-600 hover:bg-red-700 active:scale-95 transition transform duration-150 py-2 px-4 text-sm rounded-md font-semibold text-white shadow-sm"
+>
+  <TrashIcon className="w-5 h-5" />
+  
+</button>
+
+            </div>
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
